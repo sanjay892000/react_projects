d2ce2c61301e108bd4f83dfb0e7462cc0901c2e6
sanjay892000 <sanjay892000@gmail.com>
2024-11-07 00:11:48 +0530

    reactrouter create

--- 03reactrouter/src/App.jsx
+++ 03reactrouter/src/App.jsx
@@ -1,14 +1,15 @@
+
 import './App.css'
-import Footer from './components/Footer'
-import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
     <Navbar/>
-    <Home/>
+    <Outlet/>
     <Footer/>
     </>
   )
--- 03reactrouter/src/main.jsx
+++ 03reactrouter/src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import GitHub from './components/GitHub';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "",
+       element: <Home/>
+      },
+      {
+        path: '/about',
+       element: <About/>
+      },
+      {
+        path: '/contact',
+       element:<Contact/>
+      },
+      {
+        path: '/github',
+       element:<GitHub/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
