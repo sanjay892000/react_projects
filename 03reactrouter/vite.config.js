249ecd8afe78f6dc1d8a0d243996b1714d989f4f
sanjay892000 <sanjay892000@gmail.com>
2024-11-10 14:39:50 +0530

    _headers add

--- 03reactrouter/vite.config.js
+++ 03reactrouter/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use a relative base path to avoid path issues
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: 'dist',   // Output directory for Netlify
+    assetsDir: 'assets', // Directory for bundled files
   },
 })
